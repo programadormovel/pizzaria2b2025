fc95e70093e3c38c3817d8a2ea9f4640f6f00de2
programadormovel <programadormovel@gmail.com>
2025-03-31 08:33:30 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 import { useState } from "react"
 
-function App(){
+function App() {
 
-  const [nome, setNome] = useState('Ronaldo')
+ 
+
+  const Formulario = (props) => {
+
+    const [nome, setNome] = useState('Ronaldo')
+
+    return (
+      <div>
+        <h3>{props.titulo}</h3>
+        <input className="nome"
+          placeholder="Digite seu nome..."
+          onChange={(e) => { setNome(e.target.value) }}
+          name="nome"
+          type="text" />
+        <button className="botao" onClick={
+                () => {
+                  if(nome.length > 0)
+                    alert(nome) 
+                  else
+                    alert(props.mensagem)
+                }
+              }>
+          CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
 
   return (
     <div>
       <h3> Pizzaria 2B</h3>
-<input className="nome" 
-  placeholder="Digite seu nome..."
-  onChange={(e)=>{setNome(e.target.value)}}
-  name="nome"
-  type="text" />
-<button className="botao" onClick={()=>alert(nome)}>
-    CLIQUE AQUI
-</button>  
+      <Formulario titulo = "Nome" mensagem = "Santos" />
+      <Formulario titulo = "E-mail" mensagem = "Curintia" />
+      <Formulario titulo = "CPF" mensagem = "Parmera" />
+      <Formulario />
+
     </div>
   )
 }
